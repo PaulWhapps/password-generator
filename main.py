8e78b48ad6ec7f9af9b12aaa9bb284e9f7851178
PaulWhapps <78338130+PaulWhapps@users.noreply.github.com>
2021-02-02 18:49:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 import string
 
-#password generator
 class PassGenerator():
     def numpass(self): #generates random numbers
         rand = random.sample(range(0,9),6)
@@ -9,9 +8,8 @@ class PassGenerator():
         return my_str
      
     def letterpass(self):#generates random letters, upper and lower case
-        length_string = 7
         letters = string.ascii_letters
-        random_string = "".join(random.choice(letters) for i in range(length_string))
+        random_string = "".join(random.choice(letters) for i in range(7))
         return random_string
     
     def symbols(self):#selects a random symbol from the list below
@@ -20,7 +18,11 @@ class PassGenerator():
         return random_symbol
             
     def final_password(self): #prints the result of each function
-        print(self.numpass()+self.letterpass()+self.symbols())
-        
+        li = (self.numpass()+self.letterpass()+self.symbols())
+        new_li = list(li)
+        random.shuffle(new_li)
+        final_string = ''.join(new_li)
+        print(final_string)
+              
 c = PassGenerator()
 c.final_password()
